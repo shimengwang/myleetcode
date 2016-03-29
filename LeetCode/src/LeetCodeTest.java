b72ec9ab46df51845a4db72435bf52126fcf71d9
shimengwang <shimengwng165@gmail.com>
2016-03-29 23:37:09 +0800

    20160329

--- LeetCode/src/LeetCodeTest.java
+++ LeetCode/src/LeetCodeTest.java
@@ -1,9 +1,10 @@
+import java.util.HashMap;
 
 public class LeetCodeTest {
-	
-/*
- * 1. Two Sum
- * */
+
+	/*
+	 * 1. Two Sum
+	 */
 	public static int[] twoSum(int[] nums, int target) {
 		int result[] = new int[2];
 		for (int i = 0; i < nums.length; i++) {
@@ -17,12 +18,34 @@ public class LeetCodeTest {
 		return result;
 	}
 
+	public static int lengthOfLongestSubstring(String s) {
+		int maxLength = 1;
+		int result = 0;
+		int s_len = s.length();
+		HashMap<Character, Boolean> charMap = new HashMap<Character, Boolean>();
+		for (int i = 0; i < s_len; i++) {
+			maxLength = 1;
+			charMap.put(s.charAt(i), true);
+			for (int j = i + 1; j < s_len; j++) {
+				if (charMap.containsKey(s.charAt(j))) {
+					charMap.clear();
+					break;
+				} else {
+					charMap.put(s.charAt(j), true);
+					maxLength++;
+				}
+			}
+
+			if (maxLength > result) {
+				result = maxLength;
+			}
+		}
+		return result;
+	}
+
 	public static void main(String[] args) {
-		int nums[] = { 2, 7, 11, 15 };
-		int target = 18;
-		int result[] = new int[2];
-		result = twoSum(nums, target);
-		System.out.println(result[0]);
-		System.out.println(result[1]);
+		String s = "bbbbb";
+		int result = lengthOfLongestSubstring(s);
+		System.out.println(result);
 	}
 }
